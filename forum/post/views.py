824c56631779716738b1412f9cbd2de46acf3521
rensequitin <rensequitin@gmail.com>
2018-02-19 16:57:52 +0800

    Created template for post model

--- forum/post/views.py
+++ forum/post/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Post
 def index(request):
-    return HttpResponse("Hello world")
+    return showAllPosts(request)
+
+def showAllPosts(request):
+    template = loader.get_template('post/index.html')
+    all_post = Post.objects.all
+    context = { 'all_post' : all_post, }
+    return HttpResponse(template.render(context, request))
