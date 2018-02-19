bf1f18299674cac89ff4ac51ca24ba9635f5fe54
rensequitin <rensequitin@gmail.com>
2018-02-19 16:07:27 +0800

    Added post model

--- forum/post/models.py
+++ forum/post/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete = models.CASCADE)
+    post = models.CharField(max_length=500)
+    title = models.CharField(max_length=100)
