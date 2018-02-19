b4a565b446672ef3fa766c196087309794b311bb
rensequitin <rensequitin@gmail.com>
2018-02-19 17:31:43 +0800

    Created comment model

--- forum/post/models.py
+++ forum/post/models.py
@@ -5,5 +5,10 @@ class Author(models.Model):
 
 class Post(models.Model):
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
-    post = models.CharField(max_length=500)
+    post = models.CharField(max_length=600)
     title = models.CharField(max_length=100)
+
+class Comment(models.Model):
+    author = models.ForeignKey(Author, models.SET_NULL, blank=True,null=True)
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    comment = models.CharField(max_length=600)
