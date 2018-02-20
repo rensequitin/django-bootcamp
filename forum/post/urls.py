c93dd35a2a2408d07b9ec098af13368680ea910f
rensequitin <rensequitin@gmail.com>
2018-02-20 10:01:07 +0800

    With submit form

--- forum/post/urls.py
+++ forum/post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-
+app_name = "post"
 urlpatterns = [
     path('',views.index, name ='index'),
+    path('<post_id>/favorite/',views.favorite, name ='favorite'),
 ]
--- forum/post/views.py
+++ forum/post/views.py
@@ -11,3 +11,10 @@ def showAllPosts(request):
     all_post = Post.objects.all
     context = { 'all_post' : all_post, }
     return HttpResponse(template.render(context, request))
+
+def favorite(request, post_id):
+    all_post = Post.objects.all
+    post = Post.objects.get(pk=post_id)
+    add_comment = post.comment_set.create(comment=request.POST['reply'])
+    add_comment.save()
+    return render(request,'post/index.html', {'all_post':all_post})
