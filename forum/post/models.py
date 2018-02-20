ddabb6dad12bfc8ecccfcae0053a169caba9445e
rensequitin <rensequitin@gmail.com>
2018-02-20 11:53:55 +0800

    Added user info

--- forum/post/models.py
+++ forum/post/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
