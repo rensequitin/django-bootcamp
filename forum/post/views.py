5e4aa639754cf69786d09eee1511e8c34c624bde
rensequitin <rensequitin@gmail.com>
2018-02-20 12:37:18 +0800

    Created select tag for authors

--- forum/post/views.py
+++ forum/post/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Post
+from .models import Post, Author, Comment
 def index(request):
     return showAllPosts(request)
 
 def showAllPosts(request):
     template = loader.get_template('post/index.html')
     all_post = Post.objects.all
-    context = { 'all_post' : all_post, }
+    all_author = Author.objects.all
+    context = { 'all_post' : all_post, 'all_author' : all_author,}
     return HttpResponse(template.render(context, request))
 
 def favorite(request, post_id):
     all_post = Post.objects.all
+    author_info = request.POST['author_info']
+    author = Author.objects.get(pk=author_info)
+
     post = Post.objects.get(pk=post_id)
-    add_comment = post.comment_set.create(comment=request.POST['reply'])
+    add_comment = post.comment_set.create(comment=request.POST['reply'], author=author)
     add_comment.save()
     return render(request,'post/index.html', {'all_post':all_post})
