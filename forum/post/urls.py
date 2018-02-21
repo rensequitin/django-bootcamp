ec14c1430d79af28f904c2a0aaf57ea67b737f5e
rensequitin <rensequitin@gmail.com>
2018-02-21 11:52:31 +0800

    Added update function Redirect url Namespace

--- forum/post/urls.py
+++ forum/post/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
 from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 app_name = "post"
 urlpatterns = [
     path('',views.index, name ='index'),
-    path('<post_id>/favorite/',views.favorite, name ='favorite'),
-]
+    path('<post_id>/favorite/', views.favorite, name='favorite'),
+    path('update/', views.update, name='update'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- forum/post/views.py
+++ forum/post/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-
 from .models import Post, Author, Comment
+
 def index(request):
     return showAllPosts(request)
 
@@ -14,11 +14,23 @@ def showAllPosts(request):
     return HttpResponse(template.render(context, request))
 
 def favorite(request, post_id):
-    all_post = Post.objects.all
+
+    if 'author_info' not in request.POST:
+        return HttpResponseRedirect(reverse('post:index'))
+
     author_info = request.POST['author_info']
     author = Author.objects.get(pk=author_info)
-
     post = Post.objects.get(pk=post_id)
     add_comment = post.comment_set.create(comment=request.POST['reply'], author=author)
     add_comment.save()
-    return render(request,'post/index.html', {'all_post':all_post})
+    return HttpResponseRedirect(reverse('post:index'))
+
+def update(request):
+    if 'number' not in request.POST:
+        return HttpResponseRedirect(reverse('post:index'))
+
+    comment_id = request.POST['reply-id']
+    comment = Comment.objects.get(pk=comment_id)
+    comment.comment = request.POST['edit-reply']
+    comment.save()
+    return HttpResponseRedirect(reverse('post:index'))
